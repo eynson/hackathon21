114bc5c447d0ae7b5880bced0d3ef33c0c6aae26
Sebastían Girón Arcila - Ceiba Software <sebastian.giron@ceiba.com.co>
2021-11-12 17:58:53 -0500

    cambios

---
+++ src/main/java/com/example/hackathon21/Hackathon21Application.java
@@ -0,0 +1,13 @@
+package com.example.hackathon21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackathon21Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hackathon21/Hackathon21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathon21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
