6b10e30d2136aac9909d0d89600e1b0ea3e09c3e
sroba1993 <stid.rodriguez@ceiba.com.co>
2021-11-13 00:34:55 -0500

    se implementa cache en consumo

--- src/main/java/com/example/hackathon21/repositorio/InformacionRepositorioImpl.java
+++ src/main/java/com/example/hackathon21/repositorio/InformacionRepositorioImpl.java
@@ -1,11 +1,13 @@
 package com.example.hackathon21.repositorio;
 
 import com.example.hackathon21.dto.RespuestaDTO;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
@@ -17,20 +19,45 @@ public class InformacionRepositorioImpl implements IInformacionRepositorio {
 
     private static final String NUMERO = "number";
 
-    @Value("${config.base.endpoint}")
+    @Value("${config.base.endpointlocal}")
     private String url;
+
+    private Cache<String, RespuestaDTO> caffeineCache;
+
+    private static final Logger log = LoggerFactory.getLogger(InformacionRepositorioImpl.class);
+
+    public InformacionRepositorioImpl() {
+        iniciarCache(10);
+    }
 
     @Override
     public Mono<RespuestaDTO> obtenerInformacionAPI(String numeroPeticion) {
-        return registrarWebClient().get()
-                .uri(NUMERO, numeroPeticion)
-                .retrieve()
-                .bodyToMono(RespuestaDTO.class)
-                .retryWhen(Retry.backoff(5, Duration.ofSeconds(2)));
+        RespuestaDTO respuestaDtoCache = caffeineCache.getIfPresent(numeroPeticion);
+        if (respuestaDtoCache != null) {
+            return Mono.just(respuestaDtoCache);
+        } else {
+            return registrarWebClient().get()
+                    .uri(NUMERO, numeroPeticion)
+                    .accept(MediaType.APPLICATION_STREAM_JSON)
+                    .retrieve()
+                    .bodyToMono(RespuestaDTO.class)
+                    .retryWhen(Retry.backoff(3, Duration.ofSeconds(5)))
+                    .doOnNext( respuesta -> {
+                        iniciarCache(respuesta.getValiditySeconds());
+                        caffeineCache.put(numeroPeticion, respuesta);
+                    });
+        }
     }
 
     private WebClient registrarWebClient() {
         return WebClient.create(url);
+    }
+
+    private void iniciarCache(int expiracionToken){
+        caffeineCache = Caffeine.newBuilder()
+                .maximumSize(100)
+                .expireAfterWrite(Duration.ofSeconds(expiracionToken))
+                .build();
     }
 
 }
