8e15130dbb2d19a8d8dae4b94f52730a51f17f4f
sroba1993 <stid.rodriguez@ceiba.com.co>
2021-11-13 10:18:56 -0500

    cambio valores de reintento v1.2

--- src/main/java/com/example/hackathon21/repositorio/InformacionRepositorioImpl.java
+++ src/main/java/com/example/hackathon21/repositorio/InformacionRepositorioImpl.java
@@ -41,7 +41,7 @@ public class InformacionRepositorioImpl implements IInformacionRepositorio {
                     .uri(builder -> builder.queryParam(NUMERO, numeroPeticion).build())
                     .retrieve()
                     .bodyToMono(RespuestaDTO.class)
-                    .retryWhen(Retry.backoff(8, Duration.ofSeconds(4)))
+                    .retryWhen(Retry.backoff(5, Duration.ofSeconds(4)))
                     .doOnNext( respuesta -> {
                         iniciarCache(respuesta.getValiditySeconds());
                         caffeineCache.put(numeroPeticion, respuesta);
